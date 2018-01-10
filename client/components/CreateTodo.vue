c81d1e3bf99461338c6a968a955fd401099f6dd9
root <bellendirr@yandex.ru>
2018-01-10 14:26:16 +0500

    completed

--- client/components/CreateTodo.vue
+++ client/components/CreateTodo.vue
@@ -12,7 +12,7 @@
           </div>
           <div class='field'>
             <label>Project</label>
-            <input type='text' ref='project' defaultValue="">
+            <input v-model="projectText" type='text' ref='project' defaultValue="">
           </div>
           <div class='ui two button attached buttons'>
             <button class='ui basic blue button' v-on:click="sendForm">
@@ -45,6 +45,7 @@ export default {
       this.isCreating = false;
     },
     sendForm() {
+      if (this.titleText.length > 0 && this.projectText.length > 0) {
         const title = this.titleText;
         const project = this.projectText;
         this.$store.dispatch('createTodo', {
@@ -52,7 +53,10 @@ export default {
           project,
           done: false,
         });
+        this.titleText='';
+        this.projectText='';
         this.newTodoText = '';
+      }
         this.isCreating = false;
     },
   },
